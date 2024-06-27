c7d29fcebb3810162ede5d1bede829b7d715b42b
Alhassane Souare <alhassane.souare@modellium.com>
2024-06-27 01:29:04 -0400

    export pdf

--- src/app/components/creator.component.ts
+++ src/app/components/creator.component.ts
@@ -1,8 +1,7 @@
-import { Action, ItemValue, Model, QuestionFactory } from 'survey-core';
-import { CommentBrick, FlatSurvey, IDocOptions, IPdfBrick, SurveyHelper, SurveyPDF, TextBrick } from 'survey-pdf';
 import { Component, OnInit } from '@angular/core';
-import { json, surveyData, surveyJson } from '../survey';
-import { SurveyCreatorModel } from 'survey-creator-core';
+import { IDocOptions, SurveyPDF } from 'survey-pdf';
+import { json, surveyData } from '../survey';
+import { Model } from 'survey-core';
 import 'survey-core/survey.i18n';
 import 'survey-creator-core/survey-creator-core.i18n';
 
@@ -20,29 +19,13 @@ const otherChoiceText = 'Other (describe): ____________________________________'
 @Component({
   selector: "component-survey-creator",
   templateUrl: "./creator.component.html",
-  styleUrls: ["./creator.component.css"],
+  styleUrls: ["./creator.component.scss"],
 })
 export class SurveyCreatorComponent implements OnInit {
   surveyModel!: Model;
-  surveyCreatorModel!: SurveyCreatorModel;
 
   ngOnInit() {
-    const survey = new Model(surveyJson);
-    const creatorOptions = {
-      showLogicTab: true,
-      isAutoSave: true
-    };
-
-    const creator = new SurveyCreatorModel(creatorOptions);
-
-    const downloadPdfAction = new Action({
-      id: "pdf-export-edit-mode",
-      title: "Save as PDF Survey creator",
-      action: () => savePdf(json, Mode.EDIT)
-    });
-    creator.toolbarItems.unshift(downloadPdfAction);
-    creator.JSON = json;
-    this.surveyCreatorModel = creator;
+    const survey = new Model(json);
 
     survey.addNavigationItem({
       id: "pdf-export-display-mode",
@@ -50,7 +33,7 @@ export class SurveyCreatorComponent implements OnInit {
       action: () => savePdf(survey.data)
     });
 
-    survey.data = surveyData;
+    // survey.data = surveyData;
     this.surveyModel = survey;
   }
 }
@@ -60,8 +43,6 @@ const savePdf = function (surveyData: any, mode: Mode = Mode.DISPLAY) {
   const pdfWidth = !!surveyData && surveyData.pdfWidth ? surveyData.pdfWidth : 210;
   const pdfHeight = !!surveyData && surveyData.pdfHeight ? surveyData.pdfHeight : 297;
   const a4FormatMargin = 25.4;
-
-  // FlatSurvey.QUES_GAP_VERT_SCALE = mode === Mode.EDIT ? 0.0 : 1.0;
 
   const pdfDocOptions: IDocOptions = {
     format: [pdfWidth, pdfHeight],
@@ -75,61 +56,9 @@ const savePdf = function (surveyData: any, mode: Mode = Mode.DISPLAY) {
     }
   };
 
-  const surveyPdf = new SurveyPDF(surveyJson, pdfDocOptions);
+  const surveyPdf = new SurveyPDF(json, pdfDocOptions);
   surveyPdf.data = surveyData;
-  surveyPdf.showInvisibleElements = true;
   surveyPdf.mode = 'display';
-
-  surveyPdf.onRenderQuestion.add(async (survey, options) => {
-    const question = options.question;
-
-    if (question.getType() === 'boolean') {
-      const radioQuestion = QuestionFactory.Instance.createQuestion("radiogroup", `${question.no ?? ''} ${question.title}`);
-
-      radioQuestion.choices = [
-        new ItemValue(true, question.locLabelTrue?.values?.en ?? question.locLabelTrue?.values?.default ?? "Yes"),
-        new ItemValue(false, question.locLabelFalse?.values?.en ?? question.locLabelFalse?.values?.default ?? "No")
-      ];
-
-      const flatRadiogroup = options.repository.create(survey, radioQuestion, options.controller, "radiogroup");
-
-      const dropdownBrick = options.bricks.shift();
-
-      if (dropdownBrick) {
-        const point = SurveyHelper.createPoint(dropdownBrick, true, true);
-
-        const radioBricks = await flatRadiogroup.generateFlats(point);
-
-        options.bricks.forEach(blocsBriques => {
-          const briques = blocsBriques.unfold();
-          if (briques.length > 1) {
-            briques.forEach(brique => {
-              if (brique instanceof TextBrick) {
-                (brique as any).text = otherChoiceText;
-                brique.yTop += 35;
-                brique.formBorderColor = hexaColors.white;
-              } else if (brique && brique instanceof CommentBrick) {
-                // brique.formBorderColor = hexaColors.white;
-                brique.formBorderColor = 'red';
-                brique.xLeft -= 5;
-                if (mode === Mode.DISPLAY) {
-                  brique.yTop += 20;
-                  brique.yBot += 15;
-                }
-
-                // if (mode === Mode.EDIT) {
-                //   brique.yTop -= 4;
-                //   brique.yBot -= 1;
-                // }
-              }
-            });
-          }
-        });
-
-        options.bricks.push(...radioBricks);
-      }
-    }
-  });
 
   surveyPdf.save();
 };
--- src/app/survey.ts
+++ src/app/survey.ts
@@ -1,57 +1,21 @@
-export const surveyJson = {
-  "title": "test Export boolean",
-  "pages": [
-    {
-      "name": "page1",
-      "elements": [
-        {
-          "type": "boolean",
-          "name": "question1",
-          "showCommentArea": true
-        },
-        {
-          "type": "boolean",
-          "name": "question2",
-          "showCommentArea": true
-        },
-        {
-          "type": "boolean",
-          "name": "question3",
-          "showCommentArea": true
-        },
-        {
-          "type": "boolean",
-          "name": "question4"
-        }
-      ]
-    }
-  ]
-};
-
 export const surveyData = {
-  "question1": true,
-  "question1-Comment": "Lorem Ipsum est un texte d'espace réservé couramment utilisé dans les industries graphique, imprimée et éditoriale pour prévisualiser les mises en page et les maquettes visuelles.",
-  "question2": false,
-  "question2-Comment": "Lorem Ipsum est un texte d'espace réservé couramment utilisé dans les industries graphique, imprimée et éditoriale pour prévisualiser les mises en page et les maquettes visuelles.",
-  "question3": true,
-  "question3-Comment": "Lorem Ipsum est un texte d'espace réservé couramment utilisé dans les industries graphique, imprimée et éditoriale pour prévisualiser les mises en page et les maquettes visuelles.Lorem Ipsum est un texte d'espace réservé couramment utilisé dans les industries graphique, imprimée et éditoriale pour prévisualiser les mises en page et les maquettes visuelles.",
-  "question4": false
+  question1: "title test",
+  question2: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.\n\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
 }
 
 export const json = {
-  "logoPosition": "right",
   "pages": [
     {
       "name": "page1",
       "elements": [
         {
-          "type": "boolean",
-          "name": "question1"
+          "type": "text",
+          "name": "question1",
         },
         {
-          "type": "boolean",
+          "type": "comment",
           "name": "question2",
-          "showCommentArea": true
+          "rows": 8
         }
       ]
     }
